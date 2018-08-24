0e689e1a35c8b3d8eb0996556af8112b8680b6e5
riscen <riscen_@hotmail.com>
2018-08-24 10:55:09 -0500

    Update
    File update.

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,21 @@ import React from "react";
 import List from "./List";
 import Form from "./Form";
 
-const App = () => (
-  <div>
-    <div>
-      <h2>Articles</h2>
-      {console.log("About to load list")}
-      <List />;
-      {console.log("List loaded")}
-    </div>
-    {/*<div className="col-md-4 offset-md-1">
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>Articles</h2>
+          {/*<List />*/}
+        </div>
+        {/*<div className="col-md-4 offset-md-1">
       <h2>Add a new article</h2>
       <Form />
     </div>*/}
-  </div>
-);
+      </div>
+    );
+  }
+}
 
 export default App;
